1da4348c9a17b5b0fbe6f4d54ec1a60ba9af3396
Lin Vieira <lin@fnxln.com>
2024-11-01 17:43:38 -0300

    add nix-wsl

---
+++ hosts/nix-wsl/default.nix
@@ -0,0 +1,44 @@
+{ config, lib, pkgs, args, vscode-server, nixos-wsl, ... }:
+
+{
+
+  imports = [
+    vscode-server.nixosModules.default
+    nixos-wsl.nixosModules.default
+
+  ];
+  wsl.enable = true;
+  wsl.defaultUser = "lin";
+
+  services.vscode-server.enable = true;
+
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+  nixpkgs.config.allowUnfree = true;
+
+  networking.hostName = "nix-wsl";
+  users.users.lin = {
+    isNormalUser = true;
+    home = "/home/lin";
+    description = "Lin Vieira";
+    extraGroups = [ "wheel" ];
+  };
+
+
+  environment.systemPackages = with pkgs;
+    [
+      git
+      nh
+      wget
+      curl
+      nixfmt
+      neovim
+      colmena
+      nushell
+    ];
+
+  programs.nix-ld = {
+    enable = true;
+    package = pkgs.nix-ld-rs;
+  };
+  system.stateVersion = "24.05"; # Did you read the comment?
+}
---
+++ outputs/x86_64-linux/src/nix-wsl.nix
@@ -0,0 +1,36 @@
+{
+  # NOTE: the args not used in this file CAN NOT be removed!
+  # because haumea pass argument lazily,
+  # and these arguments are used in the functions like `mylib.nixosSystem`, `mylib.colmenaSystem`, etc.
+  inputs
+, lib
+, myvars
+, mylib
+, system
+, genSpecialArgs
+, ...
+} @ args:
+let
+
+  name = "nix-wsl";
+  modules = {
+    nixos-modules = map mylib.relativeToRoot [
+      # common
+      # host specific
+      "hosts/nix-wsl"
+    ];
+    home-modules = map mylib.relativeToRoot [
+      "home/base/home.nix"
+      "home/base/tui"
+      "home/base/core/editors"
+      "home/base/core/shells"
+    ];
+  };
+  systemArgs = modules // args;
+
+in
+{
+  nixosConfigurations.${name} = mylib.nixosSystem systemArgs;
+
+}
+
